33e96768c7601ce26e569a4711c8d8986f9d1c67
Legand <70213999+LegandGame@users.noreply.github.com>
2025-04-19 12:43:23 -0400

    added muteFSX and muteMusic to Globs

--- Common/Globs.gd
+++ Common/Globs.gd
@@ -5,3 +5,8 @@ const TILEWIDTH  : int = 2
 const TIME_REMAINING_MAX: int = 100
 const TIME_REMAINING_LOWER: int = 1
 const TIME_REMAINING_UPPER: int = 5
+
+var muteMusic : bool = false
+var muteSFX : bool = false
+# toggle this with the line muteSFX = not muteSFX
+# use buttons pressed signal
